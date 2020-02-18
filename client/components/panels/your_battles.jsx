d9ca56e0e82456142befa53431669636ed3ea145
Kayne Ruse <kayneruse@gmail.com>
2020-02-19 07:37:33 +1100

    Coded the battle display with dummy data

--- client/components/panels/your_battles.jsx
+++ client/components/panels/your_battles.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Dropdown } from 'react-bootstrap';
 import Button from '../button.jsx';
 
 import { setWarning } from '../../actions/warning.js';
@@ -23,13 +24,178 @@ class YourBattles extends React.Component {
 			);
 		}
 
+		const YourMovePanel = props => {
+			if (!props.move) {
+				return (
+					<div className='col' style={{flex: '0 1 auto', maxWidth: '150px'}}>
+						<Button className='centered' style={{width: '150px', background:'red', borderColor: 'pink'}}>&lt;No Move&gt;</Button>
+					</div>
+				);
+			}
+
+			return (
+				<div className='col' style={{flex: '0 1 auto', maxWidth: '150px'}}>
+					<Dropdown>
+						<Dropdown.Toggle className='centered' style={{width: '150px', background: props.move.exhausted ? 'pink' : 'lightblue'}}>{props.move.name}</Dropdown.Toggle>
+
+						<Dropdown.Menu>
+							<Dropdown.Item className={props.topAttackable ? '' : 'disabled'}>Attack Top</Dropdown.Item>
+							<Dropdown.Item className={props.bottomAttackable ? '' : 'disabled'}>Attack Bottom</Dropdown.Item>
+						</Dropdown.Menu>
+					</Dropdown>
+				</div>
+			);
+		};
+
+		const YourCreaturePanel = props => {
+			if (!props.creature) {
+				return (
+					<div className='battlePartialPanel'>
+						<Dropdown>
+							<Dropdown.Toggle>Send Out</Dropdown.Toggle>
+
+							<Dropdown.Menu>
+								{props.team.map((creature, index) => {
+									return (
+										<Dropdown.Item key={`team-${index}`}>{creature.name}</Dropdown.Item>
+									);
+								})}
+							</Dropdown.Menu>
+						</Dropdown>
+					</div>
+				);
+			}
+
+			return (
+				<div className='battlePartialPanel'>
+					<div className='break mobile show' />
+
+					<Button style={{flex: '0 1 auto'}}>↩️</Button>
+					<div className='panel'>
+						<img style={{flex: '0 1 auto'}} src={`/content/sprites/creatures/${props.creature.frontImage}`} />
+						<span><strong>{props.creature.name}</strong></span>
+					</div>
+
+
+					<div className='panel' style={{ maxWidth: '300px', flex: '1'}}>
+						<span><strong>HP</strong></span>
+						<span><strong>{props.creature.currentHP}/{props.creature.maxHP}</strong></span>
+
+						<div className='break' />
+
+						<div className='table noCollapse' style={{ maxWidth: '300px', flex: '0 1 auto'}}>
+							<div className='row'>
+								<YourMovePanel move={props.creature.moves[0]} topAttackable={props.topAttackable} bottomAttackable={props.bottomAttackable} />
+								<YourMovePanel move={props.creature.moves[1]} topAttackable={props.topAttackable} bottomAttackable={props.bottomAttackable} />
+							</div>
+
+							<div className='row'>
+								<YourMovePanel move={props.creature.moves[2]} topAttackable={props.topAttackable} bottomAttackable={props.bottomAttackable} />
+								<YourMovePanel move={props.creature.moves[3]} topAttackable={props.topAttackable} bottomAttackable={props.bottomAttackable} />
+							</div>
+						</div>
+					</div>
+
+					<Dropdown style={{flex: '0 1 auto'}}>
+						<Dropdown.Toggle>Items</Dropdown.Toggle>
+
+						<Dropdown.Menu>
+							{props.items.map((item, idx) => {
+								return (
+									<Dropdown.Item key={`item-${idx}`} className={item.exhausted ? 'disabled' : ''}>{item.name}</Dropdown.Item>
+								);
+							})}
+						</Dropdown.Menu>
+					</Dropdown>
+
+					<div className='break mobile show' />
+				</div>
+			);
+		};
+
+		const EnemyCreature = props => {
+			if (!props.creature) {
+				return (
+					<div className='panel' style={{height: 'calc(150px + 1em)'}}></div>
+				);
+			}
+
+			return (
+				<div className='panel'>
+					<img style={{flex: '0 1 auto', alignSelf: 'center'}} src={`/content/sprites/creatures/${props.creature.frontImage}`} />
+					<span><strong>{props.creature.name} {props.creature.currentHP}/{props.creature.maxHP}</strong></span>
+				</div>
+			);
+		};
+
+		/*
+
+		battle: {
+			yourTopCreature: {
+				frontImage: 'image.png',
+				name: 'nickname || species',
+				maxHP: 10,
+				currentHP: 10,
+				moves: [
+					{ name: 'Tackle', idx: 'tackle', exhausted: true }
+				]
+			},
+			yourButtonCreature: {
+				//duplicate of yourTopCreature
+			},
+
+			yourTeam: [
+				{ name: 'Creature', maxHP, currentHP },
+				//up to 6
+			],
+
+			items: [
+				{ name: 'Golden Apple', idx: 'goldenapple', exhausted: true }
+			],
+			enemyTopCreature: {
+				frontImage: 'image.png',
+				name: 'nickname || species',
+				maxHP: 10,
+				currentHP: 10,
+			},
+			enemyBottomCreature: {
+				//duplicate of enemyTopCreature
+			},
+
+			logs: [ "text" ]
+		}
+
+		*/
+
 		return (
-			<div className='panel'>
+			<div className='battleContainer'>
 				{this.props.battles.map((battle, index) => {
 					return (
-						<div key={index} className={'panel'}>
-							<p>Battle {battle.id}</p>
-							<Button onClick={e => this.resign(index)}>Resign</Button>
+						<div key={`battle-${index}`} className={'battlePanel'}>
+							<div className='table noMargin'>
+								<div className='row centered'>
+									<div className='col' style={{flex: '0 1 calc(150px * 4.5)'}}>
+										<YourCreaturePanel creature={{frontImage: 'missing.png', name: 'Nickname', maxHP: 10, currentHP: 10, moves: [{ name: 'Tackle', exhausted: false }]}} items={[{ name: 'Golden Apple', exhausted: false }]} topAttackable={true} bottomAttackable={false} />
+										<YourCreaturePanel creature={{frontImage: 'missing.png', name: 'Nickname', maxHP: 10, currentHP: 10, moves: [{ name: 'Tackle', exhausted: false }]}} items={[{ name: 'Golden Apple', exhausted: false }]} topAttackable={true} bottomAttackable={true} />
+									</div>
+
+									<div className='panel' style={{maxWidth: '350px'}}>
+										<div className='col scrollable'>
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+										</div>
+										<Button onClick={e => this.resign(index)}>Resign</Button>
+									</div>
+
+									<div className='col'>
+										<EnemyCreature creature={{ frontImage: 'missing.png', name: 'Shenron', maxHP: 10, currentHP: 5 }} />
+										<EnemyCreature creature={{ frontImage: 'missing.png', name: 'Shenron', maxHP: 10, currentHP: 5 }} />
+									</div>
+								</div>
+							</div>
 						</div>
 					);
 				})}
